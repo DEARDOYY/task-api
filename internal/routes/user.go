f0d5356e24982299a8c6b53832f382cb73bd23b0
Pinya Aungkanarungrut <88120284+DEARDOYY@users.noreply.github.com>
2026-06-18 23:04:06 +0700

    Update user.go

--- internal/routes/user.go
+++ internal/routes/user.go
@@ -9,6 +9,10 @@ import (
 func UserRoute(rg *gin.RouterGroup, h *handler.UserHandler) {
 	user := rg.Group("/users")
 	{
+		// เพิ่ม route อื่นๆ เช่น POST /users, PUT /users/:id, DELETE /users/:id ตามต้องการ
 		user.GET("/:id", h.GetUser)
+		user.GET("/", h.GetAllUsers)
+		user.PUT("/:id", h.UpdateUser)
+
 	}
 }

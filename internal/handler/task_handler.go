56407fdd3b5b60cf5f10c12711a7d54d6a35298c
Pinya Aungkanarungrut <88120284+DEARDOYY@users.noreply.github.com>
2026-06-19 14:55:48 +0700

    Update task_handler.go

--- internal/handler/task_handler.go
+++ internal/handler/task_handler.go
@@ -1,1 +1,23 @@
 package handler
+
+import (
+	"net/http"
+
+	"task-api/internal/usecase"
+)
+
+type TaskHandler interface {
+	CreateTask(w http.ResponseWriter, r *http.Request)
+}
+
+type taskHandler struct {
+	usecase usecase.TaskUsecase
+}
+
+func NewTaskHandler(usecase usecase.TaskUsecase) TaskHandler {
+	return &taskHandler{usecase: usecase}
+}
+
+func (h *taskHandler) CreateTask(w http.ResponseWriter, r *http.Request) {
+	// Implementation for creating a task
+}

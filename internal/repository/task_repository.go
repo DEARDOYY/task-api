75448aa0574b73d2977476e2ece3a75d9ed0a096
Pinya Aungkanarungrut <88120284+DEARDOYY@users.noreply.github.com>
2026-06-19 14:55:46 +0700

    Update task_repository.go

--- internal/repository/task_repository.go
+++ internal/repository/task_repository.go
@@ -1,1 +1,32 @@
 package repository
+
+import (
+	"context"
+	"task-api/internal/domain"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type TaskRepository interface {
+	Create(ctx context.Context, task *domain.Task) error
+}
+
+type taskRepository struct {
+	collection *mongo.Collection
+}
+
+func NewTaskRepository(collection *mongo.Collection) TaskRepository { // 👈 return เป็น interface
+	return &taskRepository{collection: collection}
+}
+
+func (r *taskRepository) Create(ctx context.Context, task *domain.Task) error {
+	result, err := r.collection.InsertOne(ctx, task)
+	if err != nil {
+		return err
+	}
+
+	task.ID = result.InsertedID.(primitive.ObjectID)
+
+	return nil
+}

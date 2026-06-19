64b78f7d50b219e5afcde5798e189fc62204ffcb
Pinya Aungkanarungrut <88120284+DEARDOYY@users.noreply.github.com>
2026-06-19 15:36:08 +0700

    Update task.go

--- internal/routes/task.go
+++ internal/routes/task.go
@@ -1,11 +1,14 @@
 package routes
 
 import (
+	"task-api/internal/handler"
+
 	"github.com/gin-gonic/gin"
 )
 
-func TaskRoute(rg *gin.RouterGroup) {
-	// task := rg.Group("/tasks")
-	// {
-	// }
+func TaskRoute(rg *gin.RouterGroup, h *handler.TaskHandler) {
+	task := rg.Group("/tasks")
+	{
+		task.POST("/", h.CreateTask)
+	}
 }

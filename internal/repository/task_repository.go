1bd5b865109f45f50a7ac32ce49fb21dc97955a9
Pinya Aungkanarungrut <88120284+DEARDOYY@users.noreply.github.com>
2026-06-19 16:02:01 +0700

    Update task_repository.go

--- internal/repository/task_repository.go
+++ internal/repository/task_repository.go
@@ -10,6 +10,12 @@ import (
 
 type TaskRepository interface {
 	Create(ctx context.Context, task *domain.Task) error
+	FindByID(ctx context.Context, id primitive.ObjectID) (*domain.Task, error)
+	FindByUserID(ctx context.Context, userID primitive.ObjectID) ([]*domain.Task, error)
+	FindAll(ctx context.Context) ([]*domain.Task, error)
+	FindByStatus(ctx context.Context, status string) ([]*domain.Task, error)
+	Update(ctx context.Context, task *domain.Task) error
+	Delete(ctx context.Context, id primitive.ObjectID) error
 }
 
 type taskRepository struct {
@@ -29,4 +35,92 @@ func (r *taskRepository) Create(ctx context.Context, task *domain.Task) error {
 	task.ID = result.InsertedID.(primitive.ObjectID)
 
 	return nil
+}
+
+func (r *taskRepository) FindByID(ctx context.Context, id primitive.ObjectID) (*domain.Task, error) {
+	var task domain.Task
+	err := r.collection.FindOne(ctx, primitive.M{"_id": id}).Decode(&task)
+	if err != nil {
+		return nil, err
+	}
+	return &task, nil
+}
+
+func (r *taskRepository) FindByUserID(ctx context.Context, userID primitive.ObjectID) ([]*domain.Task, error) {
+	cursor, err := r.collection.Find(ctx, primitive.M{"user_id": userID})
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+
+	var tasks []*domain.Task
+	for cursor.Next(ctx) {
+		var task domain.Task
+		if err := cursor.Decode(&task); err != nil {
+			return nil, err
+		}
+		tasks = append(tasks, &task)
+	}
+
+	if err := cursor.Err(); err != nil {
+		return nil, err
+	}
+
+	return tasks, nil
+}
+
+func (r *taskRepository) FindAll(ctx context.Context) ([]*domain.Task, error) {
+	cursor, err := r.collection.Find(ctx, primitive.M{})
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+
+	var tasks []*domain.Task
+	for cursor.Next(ctx) {
+		var task domain.Task
+		if err := cursor.Decode(&task); err != nil {
+			return nil, err
+		}
+		tasks = append(tasks, &task)
+	}
+
+	if err := cursor.Err(); err != nil {
+		return nil, err
+	}
+
+	return tasks, nil
+}
+
+func (r *taskRepository) FindByStatus(ctx context.Context, status string) ([]*domain.Task, error) {
+	cursor, err := r.collection.Find(ctx, primitive.M{"status": status})
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+
+	var tasks []*domain.Task
+	for cursor.Next(ctx) {
+		var task domain.Task
+		if err := cursor.Decode(&task); err != nil {
+			return nil, err
+		}
+		tasks = append(tasks, &task)
+	}
+
+	if err := cursor.Err(); err != nil {
+		return nil, err
+	}
+
+	return tasks, nil
+}
+
+func (r *taskRepository) Update(ctx context.Context, task *domain.Task) error {
+	_, err := r.collection.UpdateOne(ctx, primitive.M{"_id": task.ID}, primitive.M{"$set": task})
+	return err
+}
+
+func (r *taskRepository) Delete(ctx context.Context, id primitive.ObjectID) error {
+	_, err := r.collection.DeleteOne(ctx, primitive.M{"_id": id})
+	return err
 }

fdb48e6523677779451d4dd3f5e15b58aa51f4b4
Pinya Aungkanarungrut <88120284+DEARDOYY@users.noreply.github.com>
2026-06-19 15:36:11 +0700

    Update routes.go

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -9,7 +9,7 @@ import (
 func SetupRoutes(r *gin.Engine, h *internal.Handlers) {
 	api := r.Group("/api/v1")
 
-	TaskRoute(api)
+	TaskRoute(api, h.Task)
 	UserRoute(api, h.User)
 	AuthRoute(api, h.Auth)
 

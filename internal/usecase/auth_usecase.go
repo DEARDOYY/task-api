28a6f1194c96d36c2139e79a99e212e1955373b2
Pinya Aungkanarungrut <88120284+DEARDOYY@users.noreply.github.com>
2026-06-25 12:03:16 +0700

    Update auth_usecase.go

--- internal/usecase/auth_usecase.go
+++ internal/usecase/auth_usecase.go
@@ -7,6 +7,8 @@ import (
 	"task-api/internal/repository"
 	"time"
 
+	jwtPkg "task-api/pkg/jwt"
+
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -16,8 +18,19 @@ type RegisterRequest struct {
 	Password string `json:"password" binding:"required,min=6"`
 }
 
+type LoginRequest struct {
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required"`
+}
+
+type LoginResponse struct {
+	Token string       `json:"token"`
+	User  *domain.User `json:"user"`
+}
+
 type AuthUsecase interface {
 	CreateUser(ctx context.Context, req RegisterRequest) (*domain.User, error)
+	Login(ctx context.Context, req LoginRequest) (*LoginResponse, error)
 }
 
 type authUsecase struct {
@@ -49,4 +62,29 @@ func (u *authUsecase) CreateUser(ctx context.Context, req RegisterRequest) (*dom
 	}
 
 	return user, nil
+}
+
+func (u *authUsecase) Login(ctx context.Context, req LoginRequest) (*LoginResponse, error) {
+	// 1. หา user จาก email
+	user, err := u.repo.FindByEmail(ctx, req.Email)
+	if err != nil {
+		return nil, errors.New("invalid email or password")
+	}
+
+	// 2. เช็ค password ตรงไหม
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(req.Password))
+	if err != nil {
+		return nil, errors.New("invalid email or password")
+	}
+
+	// 3. สร้าง JWT token
+	token, err := jwtPkg.GenerateToken(user.ID.Hex(), user.Email, user.Role)
+	if err != nil {
+		return nil, errors.New("failed to generate token")
+	}
+
+	return &LoginResponse{
+		Token: token,
+		User:  user,
+	}, nil
 }
